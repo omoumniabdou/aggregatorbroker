693b03bbc99062fe7d5a0e0ce227e071255f088a
Othman Moumni Abdou <othman.moumniabdou@artelys.com>
2019-11-14 23:46:46 +0100

    preparing main class: reading from mqtt ok + loggin configuration

---
+++ aggregatorbrocker.py
@@ -0,0 +1,16 @@
+import logging.config
+from configparser import ConfigParser
+
+import aggregatorbrocker as agbro
+
+if __name__ == '__main__':
+    # logging
+    logging.config.fileConfig('logging.conf')
+    logger = logging.getLogger(__name__)
+
+    # configuration
+    configuration = ConfigParser()
+    configuration.read("configuration.ini")
+
+    mqtt_client = agbro.MqttClient(configuration["mqtt"])
+    sql_writer = agbro.SqlWriter(configuration["sql"])
